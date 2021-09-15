443a380ac038d1de86ea894810672505df25aab3
osama250 <81525051+MostafaNabiehMourad@users.noreply.github.com>
2021-09-15 19:03:28 +0200

    make route api to get all posts and route to get one post

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    use ApiResponseTrait;
+
+    public function index()
+    {
+        $posts = PostResource::collection(Post::get());
+        return $this->apiResponse( $posts ,'ok', 200);
+    }
+
+    public function show($id)
+    {
+            $post = Post::find($id);
+
+        if($post)
+        {
+            return $this->apiResponse( new PostResource($post) ,'ok' , 200 );
+        }
+            return $this->apiResponse( null ,'The Post Not Found',401);
+    }
+}
